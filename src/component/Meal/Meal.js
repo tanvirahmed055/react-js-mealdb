d382bad78981f7dc66b8b207aee22469e71b7919
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-25 04:14:53 +0600

    added dynamic meal image (t3)

--- src/component/Meal/Meal.js
+++ src/component/Meal/Meal.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './Meal.css';
 
 const Meal = (props) => {
-    const { idMeal, strMeal, strCategory, strArea } = props.meal;
+    const { idMeal, strMeal, strCategory, strArea, strMealThumb } = props.meal;
     return (
         <div>
-
-            <h3>Meal Name: {strMeal}</h3>
-            <p><small>Meal id: {idMeal}</small></p>
-            <p><small>Meal Category: {strCategory}</small></p>
-            <p><small>Meal Area: {strArea}</small></p>
+            <div>
+                <img src={strMealThumb} alt=""></img>
+            </div>
+            <div>
+                <h3>Meal Name: {strMeal}</h3>
+                <p><small>Meal id: {idMeal}</small></p>
+                <p><small>Meal Category: {strCategory}</small></p>
+                <p><small>Meal Area: {strArea}</small></p>
+            </div>
 
         </div>
     );
